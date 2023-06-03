1d9371dbae9bbfdbd7c13123bdeb205c4ecbba86
SamGerrick <sgcdesilva@unionbankph.com>
2023-06-03 16:25:58 +0800

    Change server url

--- src/api/index.js
+++ src/api/index.js
@@ -1,2 +1,3 @@
-export const baseUrl = "https://sarisariposbe.onrender.com"; //live server
+// export const baseUrl = "https://sarisariposbe.onrender.com"; //live server render
+export const baseUrl = "https://nutty-umbrella-yak.cyclic.app"; //live server cyclic
 // export const baseUrl = "http://localhost:5000"; //localhost
