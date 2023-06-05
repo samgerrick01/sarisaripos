c3d96be0f93ecc863cad585ad8bacf200dd43df8
SamGerrick <sgcdesilva@unionbankph.com>
2023-06-05 22:36:59 +0800

    Change server

--- src/api/index.js
+++ src/api/index.js
@@ -1,2 +1,2 @@
-// export const baseUrl = "https://nutty-umbrella-yak.cyclic.app"; //live server cyclic
-export const baseUrl = "http://localhost:5000"; //localhost
+export const baseUrl = "https://nutty-umbrella-yak.cyclic.app"; //live server cyclic
+// export const baseUrl = "http://localhost:5000"; //localhost
