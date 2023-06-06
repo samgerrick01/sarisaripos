bf33507477d381fcadc10173dc658f6399cf8783
SamGerrick <sgcdesilva@unionbankph.com>
2023-06-06 16:44:28 +0800

    Fix style of table

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,6 +1,11 @@
+import { loadingOff, loadingOn } from "../redux/loadingSlice";
 import { setUser } from "../redux/loginSlice";
 
-export const Logout = (dispatch, navigate) => {
+export const Logout = async (dispatch, navigate) => {
+  dispatch(loadingOn());
+  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+  await delay(500);
+  dispatch(loadingOff());
   dispatch(setUser({}));
   sessionStorage.removeItem("user");
   navigate("/");
